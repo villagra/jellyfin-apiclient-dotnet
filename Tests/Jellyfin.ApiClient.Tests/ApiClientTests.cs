65aa18f1cbf0a755d7659f7ab26e682255ad3e22
Jose <villagra@gmail.com>
2020-03-25 15:01:22 +0100

    Test disabled

--- Tests/Jellyfin.ApiClient.Tests/ApiClientTests.cs
+++ Tests/Jellyfin.ApiClient.Tests/ApiClientTests.cs
@@ -1,6 +1,8 @@
-using Jellyfin.ApiClient.Model;
-using MediaBrowser.Model.Session;
-using Microsoft.Extensions.Logging.Abstractions;
+//using Jellyfin.ApiClient.Model;
+//using MediaBrowser.Model.Entities;
+//using MediaBrowser.Model.Querying;
+//using MediaBrowser.Model.Session;
+//using Microsoft.Extensions.Logging.Abstractions;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using System.Threading.Tasks;
@@ -15,14 +17,34 @@ namespace Jellyfin.ApiClient.Tests
         {
             Console.WriteLine("Hello World!");
 
+            /*
             var device = new Device
             {
                 DeviceName = "My Device Name",
                 DeviceId = "My Device Id"
             };
 
-            ApiClient client = new ApiClient(NullLogger.Instance, new Uri("http://192.168.1.230:32770"), "test", device, "5.0.1");
-            var result = await client.AuthenticateUserAsync("", "");
+            ApiClient client = new ApiClient(NullLogger.Instance, new Uri("https://demo.jellyfin.org/stable/"), "test", device, "5.0.1");            
+
+            var result = await client.AuthenticateUserAsync("demo", String.Empty);
+            var views = await client.GetUserViews(result.SessionInfo.UserId);
+
+            // Get the ten most recently added items for the current user
+            var items = await client.GetItemsAsync(new ItemQuery
+            {
+                UserId = client.CurrentUserId,
+                SortBy = new[] { ItemSortBy.DateCreated },
+                SortOrder = SortOrder.Descending,
+
+                // Get media only, don't return folder items
+                Filters = new[] { ItemFilter.IsNotFolder },
+
+                Limit = 10,
+
+                // Search recursively through the user's library
+                Recursive = true
+            });*/
+
         }
     }
 }
